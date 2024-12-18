70ccf9a97a10548b4fb01a47a0d5e651fbeef5b4
Anton Vrutskyi <a.vrutskyi@steel.mil.ua>
2024-12-18 08:46:43 +0200

    Fix format warnings

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react(), tailwind()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 });
