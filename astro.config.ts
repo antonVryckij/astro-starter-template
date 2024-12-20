ba8fc93ce2a5c1b0e18508ac836dbd61126a990c
Anton Vrutskyi <a.vrutskyi@steel.mil.ua>
2024-12-20 14:29:27 +0200

    Cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,4 @@
 // @ts-check
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
@@ -9,5 +7,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [react(), tailwind()],
 });
